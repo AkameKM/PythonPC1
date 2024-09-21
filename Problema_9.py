e59116a6ba16cb312ade0cb1ce404cf016f23f64
AkameKM <167798143+AkameKM@users.noreply.github.com>
2024-09-21 03:15:13 +0000

    Problema_9

---
+++ Problema_9.py
@@ -0,0 +1,10 @@
+"""
+Escriba un programa que, dada una lista, devuelve una nueva lista cuyo contenido sea igual a la
+original pero invertida. Así, dada la lista ['Di', 'buen', 'día', 'a', 'papa'], deberá devolver ['papa', 'a',
+'día', 'buen', 'Di'].
+
+"""
+
+list = ["Di", "buen", "día", "a", "papá"]
+list.reverse()
+print(list)
